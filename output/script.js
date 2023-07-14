cd673205bc4a1e6f2c5c8090bd72a52b5cd2c7cf
anUnnecessary <raihanshamil33@gmail.com>
2023-07-14 11:48:50 +0600

    second commit

--- output/script.js
+++ output/script.js
@@ -1,3 +1,28 @@
 "use strict";
 const country = "Bangladesh";
 console.log(country);
+let playerName = "Mashrafi";
+let age = 34;
+console.log(playerName);
+console.log(age);
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(3, 4));
+// array 
+let fruits = ['apple', 'banana', 'orange'];
+let fruits2 = [];
+fruits.push();
+fruits2.push(3);
+let mixed = ['apple', 3, true, {}];
+mixed.push(4);
+mixed.push("raihan");
+// object
+let person = {
+    name: "Mashrafi",
+    age: 34,
+    isCaptain: true,
+    country: "Bangladesh"
+};
+person.name = "Ashraful";
+person.country = "India";
--- src/script.ts
+++ src/script.ts
@@ -1,2 +1,43 @@
 const country = "Bangladesh"
 console.log(country);
+
+
+let playerName = "Mashrafi";
+let age = 34;
+
+console.log(playerName);
+
+console.log(age);
+
+
+function multiply(a :number , b: number){
+    return a * b
+}
+
+console.log(multiply(3,4));
+
+
+// array 
+
+let fruits = ['apple', 'banana', 'orange'];
+let fruits2 = [];
+
+fruits.push();
+fruits2.push(3);
+
+let mixed = ['apple', 3 , true , {}];
+
+mixed.push(4);
+mixed.push("raihan");
+
+
+// object
+let person = {
+    name: "Mashrafi",
+    age: 34,
+    isCaptain: true,
+    country: "Bangladesh"
+}
+
+person.name = "Ashraful";
+person.country = "India"
